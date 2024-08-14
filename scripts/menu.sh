8262101254731f447c297507c867315e619ab718
David Reed <212698+dreed47@users.noreply.github.com>
2024-08-14 10:24:57 -0400

    updates

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -11,17 +11,22 @@ options=(
     ["Select Wakeword"]='bash -c "$(wget -qLO - https://github.com/dreed47/wyoming-satellite-scripts/raw/$WYOMING_SATELLITE_SCRIPT_RELEASE/scripts/select-wakeword.sh)"'
     ["Show current Wakeword"]='bash -c "$(wget -qLO - https://github.com/dreed47/wyoming-satellite-scripts/raw/$WYOMING_SATELLITE_SCRIPT_RELEASE/scripts/show-wakeword.sh)"'
     ["Refresh Wakeword list"]='bash -c "$(wget -qLO - https://github.com/dreed47/wyoming-satellite-scripts/raw/$WYOMING_SATELLITE_SCRIPT_RELEASE/scripts/refresh-wakeword-list.sh)"'
-    ["Update service files from github"]='bash -c "$(wget -qLO - https://github.com/dreed47/wyoming-satellite-scripts/raw/$WYOMING_SATELLITE_SCRIPT_RELEASE/scripts/update-service-files.sh)"'
     ["Edit wyoming-satellite service file"]='sudo systemctl edit --force --full wyoming-satellite.service'
+    ["Edit wyoming-wakeword service file"]='sudo systemctl edit --force --full wyoming-openwakeword.service'
+    ["Edit 2mic_leds service file"]='sudo systemctl edit --force --full 2mic_leds.service'
+    ["Update service files from github"]='bash -c "$(wget -qLO - https://github.com/dreed47/wyoming-satellite-scripts/raw/$WYOMING_SATELLITE_SCRIPT_RELEASE/scripts/update-service-files.sh)"'
 )
 
 # Array of option labels, including the Quit option
-option_labels=("Reload services"
+option_labels=(
                "Select Wakeword"
                "Show current Wakeword"
                "Refresh Wakeword list"
-               "Update service files from github"
                "Edit wyoming-satellite service file"
+               "Edit wyoming-wakeword service file"
+               "Edit 2mic_leds service file"
+               "Update service files from github"
+               "Reload services"
                "Quit")
 
 
